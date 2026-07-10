805d887af279403745bf707186a8252c1e282121
bajrayejoon <binaya.bajrashakya@gmail.com>
2026-07-10 15:24:03 +0545

    feat: configure serwist PWA with next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "localhost:3000",
     "192.168.10.53:3000",
     "http://192.168.10.53:3000",
-    "https://192.168.10.53:3000"
-  ]
+    "https://192.168.10.53:3000",
+  ],
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
